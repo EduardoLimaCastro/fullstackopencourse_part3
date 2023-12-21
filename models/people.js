253c61bffb58d009772c40150fdd4c7725d09643
EduardoLimaCastro <eduardo.alencar.castro@gmail.com>
2023-12-21 12:42:40 -0300

    UPDATES

--- models/people.js
+++ models/people.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
 
-const password = process.argv[2]
+const password = process.env.PASSWORD
 
 mongoose.set('strictQuery', false)
 
